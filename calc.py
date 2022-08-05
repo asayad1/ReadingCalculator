d232fdbe28880e0356ca49876662076550c6ad94
asayad1 <ahmadksayad@gmail.com>
2022-08-05 11:38:44 -0400

    inital commit

--- calc.py
+++ calc.py
@@ -11,7 +11,11 @@ if __name__ == "__main__":
     num_weeks = int(input("How many weeks will you dedicate to finishing the book: "))
 
     # Calculate the number of pages per day  
-    pages_per_day = (num_pages / (7 * num_weeks)).__floor__()
+    pages_per_day = (num_pages / (7 * num_weeks)).__ceil__()
 
     # Print the result
-    print (f"You need to read {pages_per_day} pages per day to finish the book in {num_weeks} week(s)!") 
+    print(f"You need to read {pages_per_day} pages per day to finish the book in {num_weeks} week(s)!") 
+    
+    # Print the number of pages per reading session 
+    for i in range(2, 4):
+        print(f"\tThat is {i} {(pages_per_day / i).__ceil__() }-page reading sessions each day!")
